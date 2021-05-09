9fb605114db6701173cb55f14714ed034304d268
Angel <22824872+Zyplos@users.noreply.github.com>
2021-05-09 09:50:29 -0500

    ChunkInfo command

--- src/main/java/dev/zyplos/loungecommuna/LoungeCommuna.java
+++ src/main/java/dev/zyplos/loungecommuna/LoungeCommuna.java
@@ -2,6 +2,7 @@ package dev.zyplos.loungecommuna;
 
 import dev.zyplos.loungecommuna.Events.PlayerJoined;
 import dev.zyplos.loungecommuna.Events.PlayerMoved;
+import dev.zyplos.loungecommuna.commands.ChunkInfo;
 import dev.zyplos.loungecommuna.commands.Claim;
 import dev.zyplos.loungecommuna.commands.Profile;
 import dev.zyplos.loungecommuna.commands.devspace;
@@ -16,6 +17,7 @@ public final class LoungeCommuna extends JavaPlugin {
         this.getCommand("claim").setExecutor(new Claim());
         this.getCommand("devspace").setExecutor(new devspace());
         this.getCommand("profile").setExecutor(new Profile());
+        this.getCommand("chunkinfo").setExecutor(new ChunkInfo());
 
         getServer().getPluginManager().registerEvents(new PlayerJoined(), this);
         getServer().getPluginManager().registerEvents(new PlayerMoved(), this);
---
+++ src/main/java/dev/zyplos/loungecommuna/commands/ChunkInfo.java
@@ -0,0 +1,64 @@
+package dev.zyplos.loungecommuna.commands;
+
+import dev.zyplos.loungecommuna.Utils;
+import dev.zyplos.loungecommuna.database.ChunkDAO;
+import dev.zyplos.loungecommuna.database.Hikari;
+import dev.zyplos.loungecommuna.database.POJOs.Chunk;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.event.HoverEvent;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
+import org.apache.commons.lang.time.DurationFormatUtils;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class ChunkInfo implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+
+            ChunkDAO chunkDao = new ChunkDAO(Hikari.getDataSource());
+            List<Chunk> chunkOwnerInfo = chunkDao.fetchByCoords(player.getChunk().getX(), player.getChunk().getZ());
+
+            if (!chunkOwnerInfo.isEmpty()) {
+                String claimedString = DurationFormatUtils.formatPeriod(
+                    chunkOwnerInfo.get(0).getClaimed_on().getTime(),
+                    System.currentTimeMillis(),
+                    "d' days,' H' hrs,' m' mins, and' s' secs'"
+                );
+
+                player.sendMessage(
+                    Utils.prefixedMessage()
+                        .append(Component.text("This chunk is owned by "))
+                        .append(Component.text(chunkOwnerInfo.get(0).getName(), TextColor.color(0xffa631)))
+                        .append(Component.text(". They claimed it "))
+                        .append(Component.text(claimedString, TextColor.color(0xffa631)))
+                        .append(Component.text(" ago. "))
+                        .append(Component.text(
+                            "Click here", TextColor.color(0xa9c8fb))
+                            .clickEvent(ClickEvent.runCommand("/profile " + chunkOwnerInfo.get(0).getName()))
+                            .hoverEvent(HoverEvent.showText(Component.text("Show " + chunkOwnerInfo.get(0).getName() + "'s profile")))
+                        )
+                        .append(Component.text(" to see their profile or do "))
+                        .append(Component.text("/profile " + chunkOwnerInfo.get(0).getName(), NamedTextColor.GREEN))
+                        .append(Component.text("."))
+                );
+            } else {
+                player.sendMessage(
+                    Utils.prefixedMessage()
+                        .append(Component.text("No one has claimed this chunk. Make it yours by doing "))
+                        .append(Component.text("/claim", NamedTextColor.GREEN))
+                        .append(Component.text("."))
+                );
+            }
+        }
+        return true;
+    }
+}
